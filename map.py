c559df3a55f8cfb25c0ca9e7d8c7a33aef6e4ced
Maksimenka Matvei <70953270+MatveiBG@users.noreply.github.com>
2023-02-28 13:31:28 +0000

    Maj Pylint + Items for dict

--- map.py
+++ map.py
@@ -1,21 +1,24 @@
+'''This module contains the Map class, used to represent a map.'''
 from utils import getch
 from coord import Coord
 
 class Map():
+    '''Used to represent a map.'''
     ground = "." # class attribute, used to describe an empty cell
     dir = {'z' : Coord(0,-1),
            's' : Coord(0,1),
            'd' : Coord(1,0),
            'q' : Coord(-1,0)} # class attribute, used to describe the directions
-    
+
     def __init__(self, size = 5, pos = Coord(1,1), hero = "@"):
         self.size = size
-        self._mat = [[Map.ground for i in range(self.size)] for j in range(self.size)] # used to initialize the map with ground cells
+        self._mat = [[Map.ground for i in range(self.size)] for j in range(self.size)] 
+        # used to initialize the map with ground cells
         self._elem = {hero : pos} # used to store the elements of the map and their position
-        for el in self._elem: # used to place the elements on the map
-            posX = self._elem[el].x
-            posY = self._elem[el].y
-            self._mat[posY][posX] = el
+        for (key, element) in self._elem.items(): # used to place the elements on the map
+            pos_x = element.x
+            pos_y = element.y
+            self._mat[pos_y][pos_x] = key
 
     def __repr__(self):
         '''Prints the map line by line.'''
@@ -26,31 +29,33 @@ class Map():
             string += "\n"
 
         return string
-    
+
     def __len__(self):
         return self.size
-    
+
     def __contains__(self, item):
-        '''Returns True if item's coordinates are in the map, or if item is in the map. Returns False otherwise.'''
+        '''Returns True if item's coordinates are in the map, or if item is in the map. 
+        Returns False otherwise.'''
         if isinstance(item, Coord):
             return (item.x >= 0 and item.x < self.size) and (item.y >= 0 and item.y < self.size)
         else:
             return item in self._elem
-        
+
     def get(self, coords):
-        '''Returns the element at the coordinates coords. Returns None if coords is not in the map.'''
+        '''Returns the element at the coordinates coords.
+        Returns None if coords is not in the map.'''
         if coords in self:
             return self._mat[coords.y][coords.x]
         else:
             return None
-        
+
     def pos(self, element):
         '''Returns the coordinates of element. Returns None if element is not in the map.'''
         if element in self._elem:
             return self._elem[element]
         else:
             return None
-        
+
     def put(self, coords, element):
         '''Puts element at the coordinates coords.'''
         if coords in self:
@@ -61,26 +66,26 @@ class Map():
         '''Removes the element at the coordinates coords.'''
         if coords in self:
             self._mat[coords.y][coords.x] = Map.ground
-            for el in self._elem:
-                if self._elem[el] == coords:
-                    del self._elem[el]
+            for key, element in self._elem.items():
+                if element == coords:
+                    del self._elem[key]
                     break
 
     def move(self, element, way):
         '''Moves element in the direction way.'''
         if element in self._elem:
-            if self.pos(element)+way in self and self.get(self.pos(element)+way) == Map.ground: # if the cell is empty
-                posIni= self.pos(element)
-                self.rm(posIni)
-                self.put(posIni+way, element)
+            if self.pos(element)+way in self and self.get(self.pos(element)+way) == Map.ground:
+                # if the cell is empty
+                pos_ini= self.pos(element)
+                self.rm(pos_ini)
+                self.put(pos_ini+way, element)
 
     def __getitem__(self, key):
         '''Returns the element at the coordinates key or the coordinates of the element key.'''
         if isinstance(key, Coord):
             return self.get(key)
-        else:
-            return self.pos(key)
-        
+        return self.pos(key)
+
     def __setitem__(self, key, value):
         '''Puts value at the coordinates key or the key to the coordinates value. 
         If the element key already exists, key is moved to coordinates value.'''
@@ -88,11 +93,12 @@ class Map():
             self.put(key, value)
         else:
             if key in self._elem:
-                posIni= self.pos(key)
-                self.rm(posIni)
+                pos_ini = self.pos(key)
+                self.rm(pos_ini)
                 self.put(value, key)
-   
+
     def play(self, hero='@'):
+        '''Plays the game.'''
         while True:
             print(self)
             self.move(hero, Map.dir[getch()])
--- rogue.py
+++ rogue.py
@@ -1,18 +1,9 @@
+'''Main module of the game. It creates the map and starts the game.'''
+
 from map import Map
 from coord import Coord
 
 if __name__ == "__main__":
     carte = Map(3, Coord(2,1))
-    print(carte)
-
-    print(carte['@'])
-    print(carte[Coord(2,1)])
-
-    carte[Coord(1,1)] = 'X'
-    print(carte)
-
-    carte['X'] = Coord(0,0)
-    print(carte)
-    
     carte.play()
     
