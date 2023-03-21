60f0f2823e1006f029391fcc260702838b6c09c8
Maksimenka Matvei <70953270+MatveiBG@users.noreply.github.com>
2023-03-21 13:04:05 +0000

    add center and intersect methods

--- Rogue_V3_(Worked_on)/room.py
+++ Rogue_V3_(Worked_on)/room.py
@@ -14,3 +14,12 @@ class Room():
         '''Returns True if item is in the room, False otherwise.'''
         return ((item.x >= self.c1.x and item.x <= self.c2.x)
                 and (item.y >= self.c1.y and item.y <= self.c2.y))
+
+    def center(self):
+        '''Returns the center of the room.'''
+        return Coord((self.c1.x + self.c2.x) // 2, (self.c1.y + self.c2.y) // 2)
+
+    def intersect(self, other):
+        '''Returns True if self intersects other, False otherwise.'''
+        return (self.c1.x <= other.c2.x and self.c2.x >= other.c1.x
+                and self.c1.y <= other.c2.y and self.c2.y >= other.c1.y)
