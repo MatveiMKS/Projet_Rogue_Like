6ef69ba6b712086d1750bba6423d296064bd3de9
Maksimenka Matvei <70953270+MatveiBG@users.noreply.github.com>
2023-05-02 11:39:25 +0000

    adds a status message to the game singleton from the meet method

--- Rogue_V4/lib/equipment.py
+++ Rogue_V4/lib/equipment.py
@@ -1,9 +1,11 @@
 ''' Equipement class '''
-from  .element   import   Element
+from .element import Element
+from .the_game import theGame
 
 class Equipment(Element):
     '''Equipement class, inherits from Element class.'''
     def meet(self, hero):
         '''Called when a Hero element meets an element.'''
         hero.take(self)
+        theGame().addMessage("You pick up a " + self)
         return True
